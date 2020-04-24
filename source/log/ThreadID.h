5cfb3cf46a867a1732b8d337935418ac564927ac
Simon Shanks <sshanks@kx.com>
2020-04-24 15:51:57 +0100

    Fixed osx build. Closes #108

--- source/log/ThreadID.h
+++ source/log/ThreadID.h
@@ -1,3 +1,17 @@
+
+#ifdef _WIN32
+
+#define THREAD_ID ::GetCurrentThreadId()
+
+#elif __linux__
+
 #include <sys/syscall.h>
 #include <unistd.h>
 #define THREAD_ID syscall(SYS_gettid)
+
+#else
+
+#include <pthread.h>
+#define THREAD_ID (long)((void*)pthread_self())
+
+#endif
