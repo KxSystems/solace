0ae2ed65e092b24679aefe0d0136c2ecb7e39fb9
Simon Shanks <59612559+sshanks-kx@users.noreply.github.com>
2026-03-23 09:57:16 +0000

    Suppress unused parameter warnings in callbacks

--- src/kdbsolace.cpp
+++ src/kdbsolace.cpp
@@ -110,6 +110,7 @@ bool storePartialWrite(char* data, int dataSize, int numWritten)
 
 static void socketWrittableCbFunc(solClient_opaqueContext_pt opaqueContext_p, solClient_fd_t fd, solClient_fdEvent_t events, void *user_p)
 {
+    (void)user_p; /* unused callback param */
     if (!BLOCKED_PARTIAL_SEND_DATA.empty())
     {
         int numWritten = send(SPAIR[1], BLOCKED_PARTIAL_SEND_DATA.c_str(), BLOCKED_PARTIAL_SEND_DATA.size(), 0);
@@ -451,6 +452,8 @@ K kdbCallback(I d)
 /* The message receive callback function is mandatory for session creation. Gets called with msgs from direct subscriptions. */
 solClient_rxMsgCallback_returnCode_t defaultSubCallback ( solClient_opaqueSession_pt opaqueSession_p, solClient_opaqueMsg_pt msg_p, void *user_p )
 {
+    (void)user_p; /* unused callback param */
+    (void)opaqueSession_p; /* unused callback param */
     KdbSolaceEvent msgAndSource;
     msgAndSource._type = DIRECT_MSG_EVENT;
     msgAndSource._event._directMsg=msg_p;
@@ -461,6 +464,7 @@ solClient_rxMsgCallback_returnCode_t defaultSubCallback ( solClient_opaqueSessio
 
 solClient_rxMsgCallback_returnCode_t guaranteedSubCallback ( solClient_opaqueFlow_pt opaqueFlow_p, solClient_opaqueMsg_pt msg_p, void *user_p )
 {
+    (void)user_p; /* unused callback param */
     KdbSolaceEvent msgAndSource;
     msgAndSource._type = QUEUE_MSG_EVENT;
     msgAndSource._event._queueMsg._msg=msg_p;
@@ -472,6 +476,7 @@ solClient_rxMsgCallback_returnCode_t guaranteedSubCallback ( solClient_opaqueFlo
 
 void flowEventCallback ( solClient_opaqueFlow_pt opaqueFlow_p, solClient_flow_eventCallbackInfo_pt eventInfo_p, void *user_p )
 {
+    (void)user_p; /* unused callback param */
     int destinationType = -1;
     const char* destinationName = "";
     solClient_destination_t destination;
@@ -536,6 +541,8 @@ void flowEventCallback ( solClient_opaqueFlow_pt opaqueFlow_p, solClient_flow_ev
  *****************************************************************************/
 void eventCallback ( solClient_opaqueSession_pt opaqueSession_p, solClient_session_eventCallbackInfo_pt eventInfo_p, void *user_p )
 {
+    (void)user_p; /* unused callback param */
+    (void)opaqueSession_p; /* unused callback param */
     if (!KDB_SESSION_EVENT_CALLBACK_FUNC.empty())
     {
         // send details to kdb+
